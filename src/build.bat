be15012fe781c0bedf48ceb6c520bd1e251c862e
MrWithrow <157441498+MrWithrow@users.noreply.github.com>
2026-09-21 16:13:04 -0500

    Changed rom name to s1erzharder.bin

--- src/build.bat
+++ src/build.bat
@@ -4,15 +4,15 @@ echo Sonic ERaZor is building...
 echo ============================================
 
 if not exist Debugger\Generated mkdir Debugger\Generated
-asm68k /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ /p sonic1.asm, s1erz.bin, Debugger\Generated\s1erz.sym, Debugger\Generated\s1erz.lst
+asm68k /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ /p sonic1.asm, s1erzharder.bin, Debugger\Generated\s1erz.sym, Debugger\Generated\s1erz.lst
 
 
 echo.
 echo ============================================
-if exist s1erz.bin (
+if exist s1erzharder.bin (
 	Debugger\convsym.exe Debugger/Generated/s1erz.sym - -inopt "/processLocals-" -out log >Debugger/Generated/s1erz.symbols.log
 	Debugger\convsym.exe _Variables.asm - -in txt -out log -inopt "/fmt='%%[A-z0-9_.]: equ $%%X'" -exclude -filter ".+_End" -range FF8000 FFFFFF >>Debugger/Generated/s1erz.symbols.log
-	Debugger\convsym.exe Debugger/Generated/s1erz.symbols.log s1erz.bin -in log -a -range 0 FFFFFF
+	Debugger\convsym.exe Debugger/Generated/s1erz.symbols.log s1erzharder.bin -in log -a -range 0 FFFFFF
 	echo Successfully built!
 ) else (
 	echo !!! BUILD FAILED !!!
